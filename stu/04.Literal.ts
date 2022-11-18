ae31b0d11f16904895888f87946e2edce98f4570
coolCicada <1475395755@qq.com>
2022-11-18 19:58:52 +0800

    feat:测试local email

--- stu/04.Literal.ts
+++ stu/04.Literal.ts
@@ -9,3 +9,29 @@
   const num = 1;
   const bool = true;
 }
+
+{
+  function add1(a: number, b: number) {
+    return a + b;
+  }
+
+  const x1 = add1(1, 1);
+
+  function add2(a: number, b = 1) {
+    return a + b;
+  }
+
+  const x2 = add2(1);
+  // const x3 = add2(1, '1');
+}
+
+{
+  type Adder = (a: number, b: number) => number
+
+  const add: Adder = (a, b) => {
+    return a + b;
+  }
+
+  const x1 = add(1, 1);
+  // const x2 = add(1, '1');
+}
